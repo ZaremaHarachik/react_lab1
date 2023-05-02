b58ea0bf2907b2d6402c59652b1aef5885e1762f
Zarema Kharachyk <zkharachyk@LABA.PSWAS.LOCAL>
2023-05-02 16:12:33 -0700

    react app started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+import TextInput from './components/TextInput';
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HelloWorld() {
+  return (
+    <div>helloWorld</div>
+  )
+}
+
+export default HelloWorld;
